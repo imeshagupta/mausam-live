cb4c7a04df1fb3573a9c78491f20927987cc0d33
esha-27 <eshagupta515@gmail.com>
2025-08-12 12:15:00 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="container-fluid bg-success p-0 mt-auto">
+      <footer className="py-3 justify-content-center">
+        <p className="text-center text-light fs-6">© made by esha ❤️</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
